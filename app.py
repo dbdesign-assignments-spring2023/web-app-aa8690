8018fb00d5d649d165a3b30b543a809c81fbeb3d
aa8690 <aa8690@nyu.edu>
2023-04-29 14:50:12 -0400

    refactor: changed mongodb connection

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect, url_for, make_response,jsonify
+from flask import Flask, render_template, request, redirect, url_for, make_response
 from markupsafe import escape
 import pymongo
 import datetime
@@ -8,19 +8,37 @@ from bson.objectid import ObjectId
 import os
 import subprocess
 from pymongo import MongoClient
-
+'''
 client = MongoClient("mongodb+srv://aa8690:AAbsm07172038@cat-database.xrbv2kl.mongodb.net/?retryWrites=true&w=majority")
 db = client.cats
 cat_collection = db['cat']
-
+'''
 # instantiate the app
 app = Flask(__name__)
+
+# load credentials and configuration options from .env file
+# if you do not yet have a file named .env, make one based on the template in env.example
+import credentials
+config = credentials.get()
+
+# turn on debugging if in development mode
+if config['FLASK_ENV'] == 'development':
+    # turn on debugging, if in development
+    app.debug = True # debug mnode
+
+# make one persistent connection to the database
+connection = pymongo.MongoClient(config['MONGO_HOST'], 27017, 
+                                username=config['MONGO_USER'],
+                                password=config['MONGO_PASSWORD'],
+                                authSource=config['MONGO_DBNAME'])
+db = connection[config['MONGO_DBNAME']] # store a reference to the database
 
 for cat in cat_collection.find():
     cat_collection.update_one(
         {'_id': cat['_id']},
         {'$set': {'orig_short_descr': cat['short_descr']}}
     )
+
 # set up the routes
 @app.route('/home')
 def home():
